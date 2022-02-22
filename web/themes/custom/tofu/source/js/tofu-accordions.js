f75ec6e0bb373521746bac30e40f8395c7c7eea4
Suzanne Strahl-Cooper <>
2022-02-22 10:18:18 -0500

    updated config for custom paragraph module

---
+++ web/themes/custom/tofu/source/js/tofu-accordions.js
@@ -0,0 +1,43 @@
+/**
+ * @file
+ * A JavaScript file to set up accordions.
+ *
+ */
+
+(function (Drupal, $) {
+
+  var event = (navigator.userAgent.match(/iPhone/i)) ? "touchstart" : "click";
+
+  /**
+   * Set the Collapse functions
+   *
+   */
+  function setAccordions(trigger,block) {
+
+    // Attach click function for button toggle
+    $(trigger).each(function() {
+
+      $(this)
+        .off(event)
+        .on(event, function(e) {
+          // Add accordion functionality
+          $(this).toggleClass('open').next(block).slideToggle('500');
+          // Amend aria values as needed
+          if ($(this).hasClass('open')) {
+            $(this).attr('aria-expanded', 'true')
+              .next(block).attr('aria-hidden', 'false');
+          } else {
+            $(this).attr('aria-expanded', 'false')
+              .next(block).attr('aria-hidden', 'true');
+          }
+        });
+    });
+
+    // Everything should be closed on page load
+    $(block).hide();
+  }
+
+  // Call function
+  setAccordions('.accordion__button', '.accordion__content');
+
+} (Drupal, jQuery));
